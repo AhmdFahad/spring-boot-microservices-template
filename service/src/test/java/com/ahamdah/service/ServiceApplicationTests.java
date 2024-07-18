b30a2e5137d376cfdff9721f7ecb5ea3e31092b4
Ahmad Fahad <68047377+AhmdFahad@users.noreply.github.com>
2024-07-18 11:07:17 +0300

    create service module

---
+++ service/src/test/java/com/ahamdah/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahamdah.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
